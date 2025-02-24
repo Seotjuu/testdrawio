be26a25af3fdcc28b05ad851137c948e0d6dd2c2
Seotjuu <sjlee@moasoftware.co.kr>
2025-02-24 16:05:50 +0900

    [Feat] 테스트 다이어그램 상수변수 추가

---
+++ app/contant/testdiagram.ts
@@ -0,0 +1,127 @@
+export const testdiagram = `<mxfile host="embed.diagrams.net" agent="Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/133.0.0.0 Safari/537.36" version="26.0.16">
+<diagram id="qbw-xSN-9GMpqes2WLxn" name="test-diagram">
+<mxGraphModel dx="1288" dy="623" grid="1" gridSize="10" guides="1" tooltips="1" connect="1" arrows="1" fold="1" page="1" pageScale="0.5" pageWidth="827" pageHeight="1169" math="0" shadow="0">
+<root>
+<mxCell id="0"/>
+<mxCell id="1" parent="0"/>
+<mxCell id="n6mYHtnTUePOS1xOoPLx-1" value="Person" style="swimlane;fontStyle=2;align=center;verticalAlign=top;childLayout=stackLayout;horizontal=1;startSize=26;horizontalStack=0;resizeParent=1;resizeLast=0;collapsible=1;marginBottom=0;rounded=0;shadow=0;strokeWidth=1;" vertex="1" parent="1">
+<mxGeometry x="250" y="80" width="160" height="138" as="geometry">
+<mxRectangle x="230" y="140" width="160" height="26" as="alternateBounds"/>
+</mxGeometry>
+</mxCell>
+<mxCell id="n6mYHtnTUePOS1xOoPLx-2" value="Name" style="text;align=left;verticalAlign=top;spacingLeft=4;spacingRight=4;overflow=hidden;rotatable=0;points=[[0,0.5],[1,0.5]];portConstraint=eastwest;" vertex="1" parent="n6mYHtnTUePOS1xOoPLx-1">
+<mxGeometry y="26" width="160" height="26" as="geometry"/>
+</mxCell>
+<mxCell id="n6mYHtnTUePOS1xOoPLx-3" value="Phone Number" style="text;align=left;verticalAlign=top;spacingLeft=4;spacingRight=4;overflow=hidden;rotatable=0;points=[[0,0.5],[1,0.5]];portConstraint=eastwest;rounded=0;shadow=0;html=0;" vertex="1" parent="n6mYHtnTUePOS1xOoPLx-1">
+<mxGeometry y="52" width="160" height="26" as="geometry"/>
+</mxCell>
+<mxCell id="n6mYHtnTUePOS1xOoPLx-4" value="Email Address" style="text;align=left;verticalAlign=top;spacingLeft=4;spacingRight=4;overflow=hidden;rotatable=0;points=[[0,0.5],[1,0.5]];portConstraint=eastwest;rounded=0;shadow=0;html=0;" vertex="1" parent="n6mYHtnTUePOS1xOoPLx-1">
+<mxGeometry y="78" width="160" height="26" as="geometry"/>
+</mxCell>
+<mxCell id="n6mYHtnTUePOS1xOoPLx-5" value="" style="line;html=1;strokeWidth=1;align=left;verticalAlign=middle;spacingTop=-1;spacingLeft=3;spacingRight=3;rotatable=0;labelPosition=right;points=[];portConstraint=eastwest;" vertex="1" parent="n6mYHtnTUePOS1xOoPLx-1">
+<mxGeometry y="104" width="160" height="8" as="geometry"/>
+</mxCell>
+<mxCell id="n6mYHtnTUePOS1xOoPLx-6" value="Purchase Parking Pass" style="text;align=left;verticalAlign=top;spacingLeft=4;spacingRight=4;overflow=hidden;rotatable=0;points=[[0,0.5],[1,0.5]];portConstraint=eastwest;" vertex="1" parent="n6mYHtnTUePOS1xOoPLx-1">
+<mxGeometry y="112" width="160" height="26" as="geometry"/>
+</mxCell>
+<mxCell id="n6mYHtnTUePOS1xOoPLx-7" value="Student" style="swimlane;fontStyle=0;align=center;verticalAlign=top;childLayout=stackLayout;horizontal=1;startSize=26;horizontalStack=0;resizeParent=1;resizeLast=0;collapsible=1;marginBottom=0;rounded=0;shadow=0;strokeWidth=1;" vertex="1" parent="1">
+<mxGeometry x="150" y="320" width="160" height="138" as="geometry">
+<mxRectangle x="130" y="380" width="160" height="26" as="alternateBounds"/>
+</mxGeometry>
+</mxCell>
+<mxCell id="n6mYHtnTUePOS1xOoPLx-8" value="Student Number" style="text;align=left;verticalAlign=top;spacingLeft=4;spacingRight=4;overflow=hidden;rotatable=0;points=[[0,0.5],[1,0.5]];portConstraint=eastwest;" vertex="1" parent="n6mYHtnTUePOS1xOoPLx-7">
+<mxGeometry y="26" width="160" height="26" as="geometry"/>
+</mxCell>
+<mxCell id="n6mYHtnTUePOS1xOoPLx-9" value="Average Mark" style="text;align=left;verticalAlign=top;spacingLeft=4;spacingRight=4;overflow=hidden;rotatable=0;points=[[0,0.5],[1,0.5]];portConstraint=eastwest;rounded=0;shadow=0;html=0;" vertex="1" parent="n6mYHtnTUePOS1xOoPLx-7">
+<mxGeometry y="52" width="160" height="26" as="geometry"/>
+</mxCell>
+<mxCell id="n6mYHtnTUePOS1xOoPLx-10" value="" style="line;html=1;strokeWidth=1;align=left;verticalAlign=middle;spacingTop=-1;spacingLeft=3;spacingRight=3;rotatable=0;labelPosition=right;points=[];portConstraint=eastwest;" vertex="1" parent="n6mYHtnTUePOS1xOoPLx-7">
+<mxGeometry y="78" width="160" height="8" as="geometry"/>
+</mxCell>
+<mxCell id="n6mYHtnTUePOS1xOoPLx-11" value="Is Eligible To Enroll" style="text;align=left;verticalAlign=top;spacingLeft=4;spacingRight=4;overflow=hidden;rotatable=0;points=[[0,0.5],[1,0.5]];portConstraint=eastwest;fontStyle=4" vertex="1" parent="n6mYHtnTUePOS1xOoPLx-7">
+<mxGeometry y="86" width="160" height="26" as="geometry"/>
+</mxCell>
+<mxCell id="n6mYHtnTUePOS1xOoPLx-12" value="Get Seminars Taken" style="text;align=left;verticalAlign=top;spacingLeft=4;spacingRight=4;overflow=hidden;rotatable=0;points=[[0,0.5],[1,0.5]];portConstraint=eastwest;" vertex="1" parent="n6mYHtnTUePOS1xOoPLx-7">
+<mxGeometry y="112" width="160" height="26" as="geometry"/>
+</mxCell>
+<mxCell id="n6mYHtnTUePOS1xOoPLx-13" value="" style="endArrow=block;endSize=10;endFill=0;shadow=0;strokeWidth=1;rounded=0;curved=0;edgeStyle=elbowEdgeStyle;elbow=vertical;" edge="1" parent="1" source="n6mYHtnTUePOS1xOoPLx-7" target="n6mYHtnTUePOS1xOoPLx-1">
+<mxGeometry width="160" relative="1" as="geometry">
+<mxPoint x="230" y="163" as="sourcePoint"/>
+<mxPoint x="230" y="163" as="targetPoint"/>
+</mxGeometry>
+</mxCell>
+<mxCell id="n6mYHtnTUePOS1xOoPLx-14" value="Professor" style="swimlane;fontStyle=0;align=center;verticalAlign=top;childLayout=stackLayout;horizontal=1;startSize=26;horizontalStack=0;resizeParent=1;resizeLast=0;collapsible=1;marginBottom=0;rounded=0;shadow=0;strokeWidth=1;" vertex="1" parent="1">
+<mxGeometry x="360" y="320" width="160" height="70" as="geometry">
+<mxRectangle x="340" y="380" width="170" height="26" as="alternateBounds"/>
+</mxGeometry>
+</mxCell>
+<mxCell id="n6mYHtnTUePOS1xOoPLx-15" value="Salary" style="text;align=left;verticalAlign=top;spacingLeft=4;spacingRight=4;overflow=hidden;rotatable=0;points=[[0,0.5],[1,0.5]];portConstraint=eastwest;" vertex="1" parent="n6mYHtnTUePOS1xOoPLx-14">
+<mxGeometry y="26" width="160" height="26" as="geometry"/>
+</mxCell>
+<mxCell id="n6mYHtnTUePOS1xOoPLx-16" value="" style="line;html=1;strokeWidth=1;align=left;verticalAlign=middle;spacingTop=-1;spacingLeft=3;spacingRight=3;rotatable=0;labelPosition=right;points=[];portConstraint=eastwest;" vertex="1" parent="n6mYHtnTUePOS1xOoPLx-14">
+<mxGeometry y="52" width="160" height="8" as="geometry"/>
+</mxCell>
+<mxCell id="n6mYHtnTUePOS1xOoPLx-17" value="" style="endArrow=block;endSize=10;endFill=0;shadow=0;strokeWidth=1;rounded=0;curved=0;edgeStyle=elbowEdgeStyle;elbow=vertical;" edge="1" parent="1" source="n6mYHtnTUePOS1xOoPLx-14" target="n6mYHtnTUePOS1xOoPLx-1">
+<mxGeometry width="160" relative="1" as="geometry">
+<mxPoint x="240" y="333" as="sourcePoint"/>
+<mxPoint x="340" y="231" as="targetPoint"/>
+</mxGeometry>
+</mxCell>
+<mxCell id="n6mYHtnTUePOS1xOoPLx-18" value="Address" style="swimlane;fontStyle=0;align=center;verticalAlign=top;childLayout=stackLayout;horizontal=1;startSize=26;horizontalStack=0;resizeParent=1;resizeLast=0;collapsible=1;marginBottom=0;rounded=0;shadow=0;strokeWidth=1;" vertex="1" parent="1">
+<mxGeometry x="538" y="80" width="160" height="216" as="geometry">
+<mxRectangle x="550" y="140" width="160" height="26" as="alternateBounds"/>
+</mxGeometry>
+</mxCell>
+<mxCell id="n6mYHtnTUePOS1xOoPLx-19" value="Street" style="text;align=left;verticalAlign=top;spacingLeft=4;spacingRight=4;overflow=hidden;rotatable=0;points=[[0,0.5],[1,0.5]];portConstraint=eastwest;" vertex="1" parent="n6mYHtnTUePOS1xOoPLx-18">
+<mxGeometry y="26" width="160" height="26" as="geometry"/>
+</mxCell>
+<mxCell id="n6mYHtnTUePOS1xOoPLx-20" value="City" style="text;align=left;verticalAlign=top;spacingLeft=4;spacingRight=4;overflow=hidden;rotatable=0;points=[[0,0.5],[1,0.5]];portConstraint=eastwest;rounded=0;shadow=0;html=0;" vertex="1" parent="n6mYHtnTUePOS1xOoPLx-18">
+<mxGeometry y="52" width="160" height="26" as="geometry"/>
+</mxCell>
+<mxCell id="n6mYHtnTUePOS1xOoPLx-21" value="State" style="text;align=left;verticalAlign=top;spacingLeft=4;spacingRight=4;overflow=hidden;rotatable=0;points=[[0,0.5],[1,0.5]];portConstraint=eastwest;rounded=0;shadow=0;html=0;" vertex="1" parent="n6mYHtnTUePOS1xOoPLx-18">
+<mxGeometry y="78" width="160" height="26" as="geometry"/>
+</mxCell>
+<mxCell id="n6mYHtnTUePOS1xOoPLx-22" value="Postal Code" style="text;align=left;verticalAlign=top;spacingLeft=4;spacingRight=4;overflow=hidden;rotatable=0;points=[[0,0.5],[1,0.5]];portConstraint=eastwest;rounded=0;shadow=0;html=0;" vertex="1" parent="n6mYHtnTUePOS1xOoPLx-18">
+<mxGeometry y="104" width="160" height="26" as="geometry"/>
+</mxCell>
+<mxCell id="n6mYHtnTUePOS1xOoPLx-23" value="Country" style="text;align=left;verticalAlign=top;spacingLeft=4;spacingRight=4;overflow=hidden;rotatable=0;points=[[0,0.5],[1,0.5]];portConstraint=eastwest;rounded=0;shadow=0;html=0;" vertex="1" parent="n6mYHtnTUePOS1xOoPLx-18">
+<mxGeometry y="130" width="160" height="26" as="geometry"/>
+</mxCell>
+<mxCell id="n6mYHtnTUePOS1xOoPLx-24" value="" style="line;html=1;strokeWidth=1;align=left;verticalAlign=middle;spacingTop=-1;spacingLeft=3;spacingRight=3;rotatable=0;labelPosition=right;points=[];portConstraint=eastwest;" vertex="1" parent="n6mYHtnTUePOS1xOoPLx-18">
+<mxGeometry y="156" width="160" height="8" as="geometry"/>
+</mxCell>
+<mxCell id="n6mYHtnTUePOS1xOoPLx-25" value="Validate" style="text;align=left;verticalAlign=top;spacingLeft=4;spacingRight=4;overflow=hidden;rotatable=0;points=[[0,0.5],[1,0.5]];portConstraint=eastwest;" vertex="1" parent="n6mYHtnTUePOS1xOoPLx-18">
+<mxGeometry y="164" width="160" height="26" as="geometry"/>
+</mxCell>
+<mxCell id="n6mYHtnTUePOS1xOoPLx-26" value="Output As Label" style="text;align=left;verticalAlign=top;spacingLeft=4;spacingRight=4;overflow=hidden;rotatable=0;points=[[0,0.5],[1,0.5]];portConstraint=eastwest;" vertex="1" parent="n6mYHtnTUePOS1xOoPLx-18">
+<mxGeometry y="190" width="160" height="26" as="geometry"/>
+</mxCell>
+<mxCell id="n6mYHtnTUePOS1xOoPLx-27" value="" style="endArrow=open;shadow=0;strokeWidth=1;rounded=0;curved=0;endFill=1;edgeStyle=elbowEdgeStyle;elbow=vertical;" edge="1" parent="1" source="n6mYHtnTUePOS1xOoPLx-1" target="n6mYHtnTUePOS1xOoPLx-18">
+<mxGeometry x="0.5" y="41" relative="1" as="geometry">
+<mxPoint x="410" y="152" as="sourcePoint"/>
+<mxPoint x="570" y="152" as="targetPoint"/>
+<mxPoint x="-40" y="32" as="offset"/>
+</mxGeometry>
+</mxCell>
+<mxCell id="n6mYHtnTUePOS1xOoPLx-28" value="0..1" style="resizable=0;align=left;verticalAlign=bottom;labelBackgroundColor=none;fontSize=12;" connectable="0" vertex="1" parent="n6mYHtnTUePOS1xOoPLx-27">
+<mxGeometry x="-1" relative="1" as="geometry">
+<mxPoint y="4" as="offset"/>
+</mxGeometry>
+</mxCell>
+<mxCell id="n6mYHtnTUePOS1xOoPLx-29" value="1" style="resizable=0;align=right;verticalAlign=bottom;labelBackgroundColor=none;fontSize=12;" connectable="0" vertex="1" parent="n6mYHtnTUePOS1xOoPLx-27">
+<mxGeometry x="1" relative="1" as="geometry">
+<mxPoint x="-7" y="4" as="offset"/>
+</mxGeometry>
+</mxCell>
+<mxCell id="n6mYHtnTUePOS1xOoPLx-30" value="lives at" style="text;html=1;resizable=0;points=[];;align=center;verticalAlign=middle;labelBackgroundColor=none;rounded=0;shadow=0;strokeWidth=1;fontSize=12;" vertex="1" connectable="0" parent="n6mYHtnTUePOS1xOoPLx-27">
+<mxGeometry x="0.5" y="49" relative="1" as="geometry">
+<mxPoint x="-38" y="40" as="offset"/>
+</mxGeometry>
+</mxCell>
+<mxCell id="n6mYHtnTUePOS1xOoPLx-31" value="Object" style="html=1;whiteSpace=wrap;" vertex="1" parent="1">
+<mxGeometry x="390" y="240" width="110" height="50" as="geometry"/>
+</mxCell>
+</root>
+</mxGraphModel>
+</diagram>
+</mxfile>`
--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,14 @@
 "use client";
 import React from 'react';
 import { DrawIoEmbed } from 'react-drawio';
+import { testdiagram } from './contant/testdiagram';
 
 const App = () => {
   return (
     <div style={{ backgroundColor: "gray", border: "1px solid black", display: "flex", flexDirection: "column", justifyContent: "center", alignItems: "center", height: "100vh" }}>
       <h1 className='text-4xl font-bold'>react-drawio library </h1>
       <div style={{ height: "80vh", width: "80%", }}>
-        <DrawIoEmbed />
+        <DrawIoEmbed xml={testdiagram} />
       </div>
     </div>
   );
