37b043c9eaf5c2b3ccdf31cf2344e0d60e267640
Seotjuu <sjlee@moasoftware.co.kr>
2025-02-24 15:13:59 +0900

    [Fix] 배경 수정

--- app/page.tsx
+++ app/page.tsx
@@ -4,9 +4,9 @@ import { DrawIoEmbed } from 'react-drawio';
 
 const App = () => {
   return (
-    <div style={{ backgroundColor: "black", border: "1px solid black", display: "flex", flexDirection: "column", justifyContent: "center", alignItems: "center", height: "100vh" }}>
-      <h1>react-drawio library Test Project </h1>
-      <div style={{ height: "50vh", width: "50%", }}>
+    <div style={{ backgroundColor: "gray", border: "1px solid black", display: "flex", flexDirection: "column", justifyContent: "center", alignItems: "center", height: "100vh" }}>
+      <h1 className='text-4xl font-bold'>react-drawio library </h1>
+      <div style={{ height: "80vh", width: "80%", }}>
         <DrawIoEmbed />
       </div>
     </div>
